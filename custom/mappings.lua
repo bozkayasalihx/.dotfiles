a7011ee673a2a4852cc2140dbfe7de96c0941ed9
bozkayasalihx <jamesgosling10@gmail.com>
2023-09-11 13:07:07 +0300

    update config

--- custom/mappings.lua
+++ custom/mappings.lua
@@ -4,7 +4,6 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
-
     ["<c-]>"] = { "<cmd> vsplit <CR>", "Split Window Verticaly" },
     ["<c-[>"] = { "<cmd> split <CR>", "Split Window Horizontally" },
   },
