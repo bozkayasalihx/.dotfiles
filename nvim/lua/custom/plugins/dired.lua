82d7c020872ab17fd6d938ee25c1bebc5789e9ca
bozkayasalihx <jamesgosling10@gmail.com>
2023-10-05 13:04:53 +0300

    udpated to v2

---
+++ nvim/lua/custom/plugins/dired.lua
@@ -0,0 +1,17 @@
+return {
+    "bozkayasalihx/dired.nvim",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+    },
+    lazy = false,
+    config = function()
+        require("dired").setup {
+            path_separator = "/",
+            show_banner = false,
+            show_hidden = true,
+            show_dot_dirs = true,
+            show_colors = true,
+        }
+    end,
+
+}
