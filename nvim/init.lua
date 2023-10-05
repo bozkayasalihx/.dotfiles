ac34516141b8314643145c0536af41ef56d137a9
bozkayasalihx <jamesgosling10@gmail.com>
2023-10-05 21:53:27 +0300

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
@@ -492,6 +493,7 @@ vim.wo.number = true
 -- Enable mouse mode
 vim.o.mouse = 'a'
 vim.o.tabstop = 4
+
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
--- nvim/lua/custom/plugins/neogit.lua
+++ nvim/lua/custom/plugins/neogit.lua
@@ -1,10 +1,10 @@
 return {
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim",           -- required
-    "nvim-telescope/telescope.nvim",   -- optional
-    "sindrets/diffview.nvim",          -- optional
-  },
-  config = true,
-  lazy = false,
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "nvim-telescope/telescope.nvim", -- optional
+      "sindrets/diffview.nvim",        -- optional
+    },
+    config = true,
+    lazy =  false,
 }
