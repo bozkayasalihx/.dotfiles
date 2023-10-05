260dd8194d23618f0158f031c23b37ee61fbc185
bozkayasalihx <jamesgosling10@gmail.com>
2023-10-05 21:41:15 +0300

    updated some configuration

--- nvim/lua/custom/plugins/nvterm.lua
+++ nvim/lua/custom/plugins/nvterm.lua
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-return  {
-    "NvChad/nvterm",
-    init = function()
-      -- require("core.utils").load_mappings "nvterm"
-    end,
-    config = function()
-      -- require "base46.term"
-      require("nvterm").setup {
-        terminals = {
-          shell = vim.o.shell,
-          list = {},
-          type_opts = {
-            float = {
-              relative = "editor",
-              row = 0.25,
-              col = 0.5,
-              width = 0.8,
-              height = 0.6,
-              border = "single",
-            },
-            horizontal = { location = "rightbelow", split_ratio = 0.3 },
-            vertical = { location = "rightbelow", split_ratio = 0.5 },
-          },
-        },
-        behavior = {
-          autoclose_on_quit = {
-            enabled = false,
-            confirm = true,
-          },
-          close_on_exit = true,
-          auto_insert = true,
-        },
-      }
-    end,
-  }
-=======
 return {
   "rsship/nvterm",
   init = function()
@@ -71,4 +34,3 @@ return {
     }
   end,
 }
->>>>>>> devel
