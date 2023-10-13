3eb19690e29471d0401d8428e45a917df3d3d80c
bozkayasalihx <bozkayasalih01x@gmail.com>
2023-10-13 15:49:41 +0300

    removed old configuration

--- custom/chadrc.lua
+++
@@ -1,8 +0,0 @@
----@type ChadrcConfig 
-local M = {}
-M.ui = {theme = 'tokyodark'}
-M.plugins = "custom.plugins"
-
--- check core.mappings for table structure
-M.mappings = require "custom.mappings"
-return M
