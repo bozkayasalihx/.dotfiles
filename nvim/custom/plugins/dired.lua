c179947fdcb0a7464cfc6ce8eb2f91508b03939e
darth_vader <bozkayasalih01x@gmail.com>
2023-11-27 13:59:08 +0300

    neovim config updated

---
+++ nvim/custom/plugins/dired.lua
@@ -0,0 +1,18 @@
+return {
+    -- dir = "/Users/salihbozkaya/Documents/ndired.nvim",
+    "X3eRo0/dired.nvim",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+    },
+    lazy = false,
+    config = function()
+        require("dired").setup {
+            path_separator = "/",
+            show_banner = false,
+            show_hidden = true,
+            show_dot_dirs = true,
+            show_colors = true,
+        }
+    end,
+
+}
